7d3cf12020e00ddee298c3df0f671e0edd8c3097
shaolianbo <lianbo8@gmail.com>
2014-09-28 11:55:45 +0800

    确定项目结构

---
+++ apphub/app_spider/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for app_spider project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'app_spider'
+
+SPIDER_MODULES = ['app_spider.spiders']
+NEWSPIDER_MODULE = 'app_spider.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'app_spider (+http://www.yourdomain.com)'
