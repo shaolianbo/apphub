b8add51792f216d949d61f522deea19fbb9bace6
shaolianbo <lianbo8@gmail.com>
2014-10-15 15:52:14 +0800

    通过apk_name , 返回app

--- apphub/app_api/views.py
+++ apphub/app_api/views.py
@@ -22,10 +22,24 @@ class PermissionViewSet(viewsets.ReadOnlyModelViewSet):
 class AppIdentificationViewSet(viewsets.ReadOnlyModelViewSet):
     model = AppIdentification
 
+    def get_queryset(self):
+        queryset = AppIdentification.objects.all()
+        apk_name = self.request.QUERY_PARAMS.get('apk_name', None)
+        if apk_name:
+            queryset = queryset.filter(apk_name=apk_name)
+        return queryset
+
 
 class AppViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = AppInfo.objects.all()
     serializer_class = AppSerializer
+
+    def get_queryset(self):
+        queryset = AppInfo.objects.all()
+        apk_name = self.request.QUERY_PARAMS.get('apk_name', None)
+        if apk_name:
+            queryset = queryset.filter(app_id__apk_name=apk_name)
+        return queryset
 
 
 class ScreenshotViewSet(viewsets.ReadOnlyModelViewSet):
