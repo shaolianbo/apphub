f2de736224fb02130935b9c045d5b0f6bb615343
shaolianbo <lianbo8@gmail.com>
2014-10-23 13:31:38 +0800

    api接口AppInfo的信息输出扁平化

--- apphub/app_api/serializers.py
+++ apphub/app_api/serializers.py
@@ -1,18 +1,35 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework.serializers import HyperlinkedModelSerializer, SerializerMethodField
 
 from store.models import (
     AppInfo, Category, Screenshot
 )
 
 
-class AppSerializer(HyperlinkedModelSerializer):
-    class Meta:
-        model = AppInfo
-
-
 class CategorySerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Category
+
+
+class AppSerializer(HyperlinkedModelSerializer):
+    category = SerializerMethodField('get_category_name')
+    tags = SerializerMethodField('get_tags_names')
+    screen_shots = SerializerMethodField('get_screen_shots')
+    app_id = SerializerMethodField('get_apk_name')
+
+    class Meta:
+        model = AppInfo
+
+    def get_category_name(self, obj):
+        return obj.category.name
+
+    def get_tags_names(self, obj):
+        return [tag.name for tag in obj.tags.all()]
+
+    def get_screen_shots(self, obj):
+        return obj.screenshot_set.all().values('image', 'origin_url')
+
+    def get_apk_name(self, obj):
+        return obj.app_id.apk_name
 
 
 class ScreenshotSerializer(HyperlinkedModelSerializer):
--- apphub/apphub/urls.py
+++ apphub/apphub/urls.py
@@ -5,6 +5,6 @@ from app_api.urls import router
 
 urlpatterns = patterns(
     '',
-    url(r'^api/', include(router.urls, namespace='spider')),
+    url(r'^api/', include(router.urls)),
     url(r'^spider/', include('app_spider.urls', namespace='spider'))
 )
