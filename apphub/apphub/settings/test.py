0125a22574aca21233cc62b02f13a6f120ebb818
shaolianbo <lianbo8@gmail.com>
2015-12-15 23:19:10 +0800

    去除一些配置信息

--- apphub/apphub/settings/test.py
+++ apphub/apphub/settings/test.py
@@ -4,10 +4,10 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'apphub',
-        'USER': 'solar',
-        'PASSWORD': '!@#solar',
-        'HOST': '10.13.85.89',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
         'PORT': 3306,
     }
 }
@@ -17,4 +17,4 @@ REST_FRAMEWORK.update({
     'DEFAULT_RENDERER_CLASSES': ('rest_framework.renderers.JSONRenderer',)
 })
 
-DATA_SYNC_API = "http://t2.ams.sohuno.com/api/sync_from_spider"
+DATA_SYNC_API = ""
--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -11,23 +11,23 @@ from essay.tasks import deploy
 #from essay.tasks import nginx
 #from essay.tasks import supervisor
 
-env.GIT_SERVER = 'git.m.sohuno.com'  # ssh地址只需要填：github.com
+env.GIT_SERVER = ''  # ssh地址只需要填：github.com
 env.PROJECT = 'apphub'
-env.BUILD_PATH = '/home/solar/build/'
+env.BUILD_PATH = ''
 env.PROJECT_OWNER = 'lianboshao'
 env.DEFAULT_BRANCH = 'master'
-env.PYPI_INDEX = 'http://10.13.85.57:12180/simple'
+env.PYPI_INDEX = ''
 
 
 ######
 # deploy settings:
 env.PROCESS_COUNT = 2  #部署时启动的进程数目
 env.roledefs = {
-    'build': ['solar@10.10.81.160'],  # 打包服务器配置
-    'test': ['solar@10.10.93.39'],
+    'build': [''],  # 打包服务器配置
+    'test': [''],
 }
 
-env.VIRTUALENV_PREFIX = '/home/solar/apphub'
+env.VIRTUALENV_PREFIX = ''
 env.SUPERVISOR_CONF_TEMPLATE = os.path.join(PROJECT_ROOT, 'conf', 'supervisord.conf')
 
 # 根据工程确定项目编号
@@ -43,9 +43,3 @@ env.VENV_PORT_PREFIX_MAP = {
     'h': '%d7' % PROJECT_NUM,
     'i': '%d8' % PROJECT_NUM,
 }
-
-# env.NGINX_BIN = '/opt/apps/nginx/sbin/nginx'
-# env.NGINX_CONF = '/opt/apps/nginx/conf/nginx.conf'
-# env.NGINX_SWITCH_CONF = '/opt/apps/nginx/conf/nginx.conf'
-# A环境配置:"upstream_pvrs_A"
-# B环境配置:"upstream_pvrs_B"
